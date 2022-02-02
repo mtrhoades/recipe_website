21a8ff249ed810902b9c9964c62b229edeb260c2
root <mtrhoades0727@gmail.com>
2022-02-02 00:07:37 -1000

    picked color palette, chose font, started JS

--- code.js
+++ code.js
@@ -1,1 +1,9 @@
 console.log("hello")
+
+let header1 = document.createElement('h1');
+header1.textContent = 'Hello World';
+document.body.append(header1);
+
+header1.addEventListener('dblclick', function() {
+	alert("I was double-clicked!");
+});
